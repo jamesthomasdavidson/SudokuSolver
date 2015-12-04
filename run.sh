04ba100329d217fba8959b10d59376749e8a3ae0
James Davidson <jamesdav@uvic.ca>
2015-12-04 15:46:51 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 for f in tests/unencoded/*
 do
-	python sudokusolver.py encode $f
 	minisatinput="tests/encoded/"$(basename $f)
 	minisatoutput="tests/decode/"$(basename $f)
+	python sudokusolver.py encode $f $minisatinput
 	minisat $minisatinput $minisatoutput 
 	python sudokusolver.py decode $minisatoutput
 	echo "\n\n"
