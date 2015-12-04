7092f826ab924cc4929d97775cc793ace9aa2293
William Lynch <w4lynch@gmail.com>
2015-12-04 13:45:26 -0800

    Writes encoding to file

--- sudukosolver.py
+++ sudukosolver.py
@@ -2,10 +2,10 @@
 James Davidson - V00812527 | Brdiget Rassell - V00804180 | Wilfred Lynch - V00809085
 SAT Suduko Solver implemented in python
 """
-import pycosat
 
+import sys
 
-def variable(i, j, d):
+def base_nine(i, j, d):
 	return 81 * (i - 1) + 9 * (j - 1) + (d - 1) + 1
 
 def clauses():
@@ -14,11 +14,11 @@ def clauses():
 	for i in range(1, 10):
 		for j in range(1, 10):
 			""" 1 | Assign a number to every square """
-			clauselist.append([variable(i, j, d) for d in range(1,10)])
+			clauselist.append([base_nine(i, j, d) for d in range(1, 10)])
 			for d in range(1,10):
 				for l in range(d+1, 10):
 					""" 36 | but make sure that no square has the same number """
-					clauselist.append([-variable(i,j,d), -variable(i,j,l)])
+					clauselist.append([-base_nine(i, j, d), -base_nine(i, j, l)])
 
 	def valid(cells):
 		for i, ei in enumerate(cells):
@@ -26,7 +26,7 @@ def clauses():
 				if i<j:
 					for d in range(1,10):
 						""" # | make sure that the same number doesn't exist more than once in the """
-						clauselist.append([-variable(ei[0],ei[1],d), -variable(ej[0],ej[1],d)])
+						clauselist.append([-base_nine(ei[0], ei[1], d), -base_nine(ej[0], ej[1], d)])
 
 	for i in range(1,10):
 		valid([(i,j) for j in range(1,10)])
@@ -38,24 +38,24 @@ def clauses():
 
 	return clauselist
 
-def solvePuzzle(grid):
+def encodePuzzle(grid):
 	cnf = clauses()
 	for i in range(1,10):
 		for j in range(1,10):
-			d = grid[i-i][j-1]
+			d = grid[i-1][j-1]
 			if d:
-				cnf.append(variable(i,j,d))
-	
-	solution = pycosat.solve(cnf)
+				cnf.append([base_nine(i, j, d)])
+	return cnf
+	#solution = pycosat.solve(cnf)
 
-	def getcellnumber(i,j):
-		for d in range(1,10):
-			if variable(i,j,d) in solution:
-				return d
-
-	for i in range(1,10):
-		for j in range(1,10):
-			grid[i-1][j-1] = getcellnumber(i,j)
+	# def getcellnumber(i,j):
+	# 	for d in range(1,10):
+	# 		if base_nine(i, j, d) in solution:
+	# 			return d
+    #
+	# for i in range(1,10):
+	# 	for j in range(1,10):
+	# 		grid[i-1][j-1] = getcellnumber(i,j)
 
 def inputToArray(input):
 	puzzle =[]
@@ -72,14 +72,47 @@ def inputToArray(input):
 def printPuzzle(puzzle):
 	for eachrow in puzzle:
 		for eachint in eachrow:
-			print(eachint + " ")
+			print(str(eachint) + " ")
 		print("")
 
-def main():
-	s = "1638.5.7..*8040065005007008450082039301000040700000000839050000604200590000093081"
-	puzzle = inputToArray(s)
-	solvePuzzle(puzzle)
-	printpuzzle(puzzle)
-	return
+def writeEncodingToFile(cnf_list, output_file_name):
+	output_file = open(output_file_name, "w")
+	output_file.write("")
+	number_of_clauses = len(cnf_list)
+	number_of_variables = base_nine(9,9,9)
+	output_file.write("p cnf " + str(number_of_variables) + " " + str(number_of_clauses) + "\n")
+	for clause in cnf_list:
+		for num in clause:
+			output_file.write(str(num) + " ")
+		output_file.write("0\n")
 
-main()
+
+def main():
+	if len(sys.argv) != 3:
+		print "Improper number of cmd arguments. See README."
+		sys.exit()
+	if sys.argv[1] == 'encode':
+		input_file = open(sys.argv[2], "r")
+		input_string = input_file.read()
+		input_string_formatted = input_string.replace("\n", "").replace(" ", "")
+		if len(input_string_formatted) != 9**2:
+			print("Input file wrong length")
+			sys.exit()
+		#print input_string_formatted
+		puzzle = inputToArray(input_string_formatted)
+		cnf_list = encodePuzzle(puzzle)
+		#print cnf_list
+		output_file_name = sys.argv[2] + "CNF"
+		writeEncodingToFile(cnf_list, output_file_name)
+
+	elif sys.argv[1] == 'decode':
+		print 'decoding'
+	else:
+		print ("improper command line argument " + sys.argv[1])
+
+	#printPuzzle(puzzle)
+
+
+
+if __name__ == "__main__":
+	main()
